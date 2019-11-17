0227a6bf2a617a3a828a9c4bc5a7ab69540d5fa1
L.R <shadowlr@sina.com>
2019-11-17 18:01:43 +0800

    feat(components): 订单页

--- app/components/fileGenerate/inputOrder/FileOrderInput.js
+++ app/components/fileGenerate/inputOrder/FileOrderInput.js
@@ -4,13 +4,18 @@ import { Table, Button } from 'antd';
 import CategoryTag from '../../common/CategoryTag';
 
 class FileOrderInput extends Component {
+
+  addOrder = () => {
+
+  }
+
   render() {
     const { data, goodsMap } = this.props;
     const dataSource = data.map(d => ({
       key: `record_${d.goods_id}`,
       goods: goodsMap[d.goods_id].name, 
-      category: d.category_id,
-      max_count: d.max_count,
+      category: goodsMap[d.goods_id].category_id,
+      max_count: goodsMap[d.goods_id].max_count,
       count: d.count,
       order_number: d.order_number,
     }))
@@ -54,20 +59,50 @@ class FileOrderInput extends Component {
         align: 'right',
       }
     ];
+    const orderColumns = [{
+      title: '',
+      width: 10,
+      key: 'index',
+      render: (text,record,index) => `${index + 1}`
+    }, {
+      title: '订单号',
+      width: 50,
+      key: 'order_number',
+      dataIndex: 'order_number',
+    }, {
+      title: '包含商品',
+      width: 150,
+      key: 'goods',
+      dataIndex: 'goods',
+    }];
+    const orderDataSource = [];
 
     return (
       <>
-        <Table
-          className="file-data-input"
-          columns={columns}
-          dataSource={dataSource}
-          scroll={{ x: '100%', y: 'calc(100vh - 210px)' }}
-          bordered
-          pagination={false}
-          size="small"
-          style={{height: 'calc(100vh - 180px)'}}
-        />
-        <Button block type="primary" style={{marginTop: 15}} onClick={this.validateOpen}>完成</Button>
+        <div style={{height: 'calc(100vh - 180px)'}}>
+          <Table
+            className="file-data-input"
+            columns={columns}
+            dataSource={dataSource}
+            scroll={{ x: '100%', y: 'calc(100vh - 400px)' }}
+            bordered
+            pagination={false}
+            size="small"
+          />
+          <div style={{marginTop: 10, marginBottom: 10, overflow: 'hidden'}}>
+            <Button type="primary" style={{float: 'right'}} onClick={this.addOrder}>新增订单</Button>
+          </div>
+          <Table
+            className="file-data-input"
+            columns={orderColumns}
+            dataSource={orderDataSource}
+            scroll={{ x: '100%', y: 'calc(100vh - 900px)' }}
+            bordered
+            pagination={false}
+            size="small"
+          />
+        </div>
+        <Button block type="success" style={{marginTop: 15}} onClick={this.validateOpen}>完成</Button>
       </>
     )
   }
